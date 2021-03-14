ced9a8b5b25bd7a8fb419366e28aafbe10de4671
Chenguang Wang <464039958@qq.com>
2021-03-15 00:39:06 +0800

    first commit

---
+++ src/main/java/com/whyischen/initializer/InitializerApplication.java
@@ -0,0 +1,13 @@
+package com.whyischen.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitializerApplication.class, args);
+	}
+
+}
